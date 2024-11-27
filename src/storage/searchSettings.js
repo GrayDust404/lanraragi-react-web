aaf0e85c6964743535da74a7fbf70291686b426f
GrayDust404 <yuanbin404@gmail.com>
2024-11-27 22:29:42 +0800

    remove some comment

--- src/storage/searchSettings.js
+++ src/storage/searchSettings.js
@@ -1,12 +1,10 @@
 import { USE_PAGINATED_SEARCH } from "./constants";
 
-// 保存设置
 export const saveSearchSettings = (settings) => {
   localStorage.setItem(USE_PAGINATED_SEARCH, JSON.stringify(settings));
 };
 
-// 加载设置
 export const loadSearchSettings = () => {
   const savedSetting = localStorage.getItem(USE_PAGINATED_SEARCH);
-  return savedSetting ? JSON.parse(savedSetting) : true; // 默认为 true
+  return savedSetting ? JSON.parse(savedSetting) : true; 
 }; 
